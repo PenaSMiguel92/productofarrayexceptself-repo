e69d59a160b0ae1ce57a1f1016a1d88ed7428872
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 15:27:51 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+import java.util.*;
 public class Main {
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.productExceptSelf(new int[] { 1, 2, 3, 4 })));
+        System.out.println(Arrays.toString(sol.productExceptSelf(new int[] {-1, 1, 0, -3, 3})));
+    }
 } 
