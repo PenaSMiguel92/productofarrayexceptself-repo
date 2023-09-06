a9a4c95a6e0b2f61bd760592b831ccb684623ac5
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-09-06 15:21:51 -0700

    first attempt

--- Solution.java
+++ Solution.java
@@ -1,4 +1,23 @@
 public class Solution {
-public int[] productExceptSelf(int[] nums) {
-}
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            int val = 1;
+            left = 0;
+            right = nums.length - 1;
+            while (left != i || right != i) {
+                if (left < i) {
+                    val *= nums[left++];
+                }
+
+                if (right > i) {
+                    val *= nums[right--];
+                }
+            }
+            nums[i] = val;
+        }
+        return answer;
+    }
 } 
